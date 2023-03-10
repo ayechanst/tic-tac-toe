1a4902035aadc80d660dbf4b6bd7cabb7e0518b8
Aye Chan <ayechan.sant@gmail.com>
2023-03-10 07:09:55 -0800

    the board

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-gameBoard = {};
+const Abuttons = document.querySelectorAll('button');
+
+Abuttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    alert('you will die');
+  });
+});
+
+const buttons = {
+  topLeft: document.getElementById('top-left'),
+  topMid: document.getElementById('top-mid'),
+  topRight: document.getElementById('top-right'),
+  midLeft: document.getElementById('mid-left'),
+  center: document.getElementById('center'),
+  midRight: document.getElementById('mid-right'),
+  bottomLeft: document.getElementById('bottom-left'),
+  bottomMid: document.getElementById('bottom-mid'),
+  bottomRight: document.getElementById('bottom-right'),
+  clear: document.getElementById('clear'),
+  playAgain: document.getElementById('play-again'),
+};
+
+let gameData = {
+  gameBoard: [
+    null, // top left
+    null, // top mid
+    null, // top right
+    null, // mid left
+    null, // mid mid
+    null, // mid right
+    null, // bottom left
+    null, // bottom mid
+    null, // bottom right
+  ],
+};
+
+function turn() {
+  let counter = 0;
+  counter++;
+  if (counter % 2 === 0) {
+    innerHTML = 'X';
+  } else {
+    innerHTML = 'O';
+  }
+}
+
+function place() {}
